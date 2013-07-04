ebb3a89693e81774166c642107d9477007198e89
Nico Ni <cedics2010@gmail.com>
2013-07-04 12:02:24 +0200

    array

---
+++ GdI2_Übung01/src/arrayListen/ArrayList.java
@@ -0,0 +1,42 @@
+package arrayListen;
+
+import listPackage.List;
+import listPackage.ListItem;
+
+public class ArrayList<T> {
+
+	private List<KeyItem<T>> list;
+	private int N;
+	
+	/**
+	 * constructor
+	 */
+	public ArrayList(int N){
+		
+		this.list = new List<KeyItem<T>>();
+		this.N=N;
+	}
+	
+	public void compress(){
+		ListItem<KeyItem<T>> p = list.first;
+
+		if(p.key.n > 0)
+			
+		while(true){
+			if(p.key.n < (N)){
+				if(p.next == null){
+					return;
+				}else{
+					p.key.array[p.key.n - 1] = p.next.key.array[0];
+				}
+			}else{
+				p = p.next;
+			}
+			
+			
+					
+				
+			
+		}
+	}
+}
---
+++ GdI2_Übung01/src/arrayListen/KeyItem.java
@@ -0,0 +1,15 @@
+package arrayListen;
+
+public class KeyItem<T> {
+
+	T[] array;
+	int n;
+	
+	/**
+	 * constructor
+	 */
+	public KeyItem(T[] array,int n){
+		this.array = array;
+		this.n = n;
+	}
+}
